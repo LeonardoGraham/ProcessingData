983a7edecbf3c44faa40729ac458915741fcd985
LeonardoGraham <leonardostefangraham@gmail.com>
2021-11-01 22:51:03 -0400

    Add project files.

---
+++ ProcessingData/Form1.Designer.cs
@@ -0,0 +1,307 @@
+﻿
+namespace ProcessingData
+{
+    partial class vehiclePriceCalc
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.stereoSystemCheckBox = new System.Windows.Forms.CheckBox();
+            this.leatherInterionCheckBox = new System.Windows.Forms.CheckBox();
+            this.basePriceTextBox = new System.Windows.Forms.TextBox();
+            this.computerNavigationCheckBox = new System.Windows.Forms.CheckBox();
+            this.standardCheckBox = new System.Windows.Forms.CheckBox();
+            this.PearlizeCheckBox = new System.Windows.Forms.CheckBox();
+            this.customizedDetailingCheckBox = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tradeInTextBox = new System.Windows.Forms.TextBox();
+            this.amountDueLabel = new System.Windows.Forms.Label();
+            this.totalLabel = new System.Windows.Forms.Label();
+            this.subtotalLabel = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.calculateToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clearToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // stereoSystemCheckBox
+            // 
+            this.stereoSystemCheckBox.AutoSize = true;
+            this.stereoSystemCheckBox.Location = new System.Drawing.Point(12, 92);
+            this.stereoSystemCheckBox.Name = "stereoSystemCheckBox";
+            this.stereoSystemCheckBox.Size = new System.Drawing.Size(100, 19);
+            this.stereoSystemCheckBox.TabIndex = 0;
+            this.stereoSystemCheckBox.Text = "Stereo System";
+            this.stereoSystemCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // leatherInterionCheckBox
+            // 
+            this.leatherInterionCheckBox.AutoSize = true;
+            this.leatherInterionCheckBox.Location = new System.Drawing.Point(12, 117);
+            this.leatherInterionCheckBox.Name = "leatherInterionCheckBox";
+            this.leatherInterionCheckBox.Size = new System.Drawing.Size(106, 19);
+            this.leatherInterionCheckBox.TabIndex = 1;
+            this.leatherInterionCheckBox.Text = "Leather Interior";
+            this.leatherInterionCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // basePriceTextBox
+            // 
+            this.basePriceTextBox.Location = new System.Drawing.Point(124, 33);
+            this.basePriceTextBox.Name = "basePriceTextBox";
+            this.basePriceTextBox.Size = new System.Drawing.Size(100, 23);
+            this.basePriceTextBox.TabIndex = 2;
+            this.basePriceTextBox.Text = "0";
+            this.basePriceTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.basePriceTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.basePriceTextBox_KeyPress);
+            // 
+            // computerNavigationCheckBox
+            // 
+            this.computerNavigationCheckBox.AutoSize = true;
+            this.computerNavigationCheckBox.Location = new System.Drawing.Point(12, 142);
+            this.computerNavigationCheckBox.Name = "computerNavigationCheckBox";
+            this.computerNavigationCheckBox.Size = new System.Drawing.Size(141, 19);
+            this.computerNavigationCheckBox.TabIndex = 3;
+            this.computerNavigationCheckBox.Text = "Computer Navigation";
+            this.computerNavigationCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // standardCheckBox
+            // 
+            this.standardCheckBox.AutoSize = true;
+            this.standardCheckBox.Location = new System.Drawing.Point(158, 92);
+            this.standardCheckBox.Name = "standardCheckBox";
+            this.standardCheckBox.Size = new System.Drawing.Size(73, 19);
+            this.standardCheckBox.TabIndex = 4;
+            this.standardCheckBox.Text = "Standard";
+            this.standardCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // PearlizeCheckBox
+            // 
+            this.PearlizeCheckBox.AutoSize = true;
+            this.PearlizeCheckBox.Location = new System.Drawing.Point(158, 117);
+            this.PearlizeCheckBox.Name = "PearlizeCheckBox";
+            this.PearlizeCheckBox.Size = new System.Drawing.Size(66, 19);
+            this.PearlizeCheckBox.TabIndex = 5;
+            this.PearlizeCheckBox.Text = "Pearlize";
+            this.PearlizeCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // customizedDetailingCheckBox
+            // 
+            this.customizedDetailingCheckBox.AutoSize = true;
+            this.customizedDetailingCheckBox.Location = new System.Drawing.Point(158, 142);
+            this.customizedDetailingCheckBox.Name = "customizedDetailingCheckBox";
+            this.customizedDetailingCheckBox.Size = new System.Drawing.Size(139, 19);
+            this.customizedDetailingCheckBox.TabIndex = 6;
+            this.customizedDetailingCheckBox.Text = "Customized Detailing";
+            this.customizedDetailingCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 15);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Base Price";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 15);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Trade In Allowance";
+            // 
+            // tradeInTextBox
+            // 
+            this.tradeInTextBox.Location = new System.Drawing.Point(124, 63);
+            this.tradeInTextBox.Name = "tradeInTextBox";
+            this.tradeInTextBox.Size = new System.Drawing.Size(100, 23);
+            this.tradeInTextBox.TabIndex = 10;
+            this.tradeInTextBox.Text = "0";
+            this.tradeInTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.tradeInTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tradeInTextBox_KeyPress);
+            // 
+            // amountDueLabel
+            // 
+            this.amountDueLabel.AutoSize = true;
+            this.amountDueLabel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.amountDueLabel.Location = new System.Drawing.Point(12, 206);
+            this.amountDueLabel.Name = "amountDueLabel";
+            this.amountDueLabel.Size = new System.Drawing.Size(115, 21);
+            this.amountDueLabel.TabIndex = 11;
+            this.amountDueLabel.Text = "Amount Due: ";
+            // 
+            // totalLabel
+            // 
+            this.totalLabel.AutoSize = true;
+            this.totalLabel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.totalLabel.Location = new System.Drawing.Point(12, 185);
+            this.totalLabel.Name = "totalLabel";
+            this.totalLabel.Size = new System.Drawing.Size(56, 21);
+            this.totalLabel.TabIndex = 12;
+            this.totalLabel.Text = "Total: ";
+            // 
+            // subtotalLabel
+            // 
+            this.subtotalLabel.AutoSize = true;
+            this.subtotalLabel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.subtotalLabel.Location = new System.Drawing.Point(12, 164);
+            this.subtotalLabel.Name = "subtotalLabel";
+            this.subtotalLabel.Size = new System.Drawing.Size(83, 21);
+            this.subtotalLabel.TabIndex = 13;
+            this.subtotalLabel.Text = "Subtotal: ";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem1,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(331, 24);
+            this.menuStrip1.TabIndex = 14;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.E)));
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // editToolStripMenuItem1
+            // 
+            this.editToolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.calculateToolStripMenuItem,
+            this.clearToolStripMenuItem});
+            this.editToolStripMenuItem1.Name = "editToolStripMenuItem1";
+            this.editToolStripMenuItem1.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.R)));
+            this.editToolStripMenuItem1.Size = new System.Drawing.Size(39, 20);
+            this.editToolStripMenuItem1.Text = "Edit";
+            // 
+            // calculateToolStripMenuItem
+            // 
+            this.calculateToolStripMenuItem.Name = "calculateToolStripMenuItem";
+            this.calculateToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.R)));
+            this.calculateToolStripMenuItem.Size = new System.Drawing.Size(164, 22);
+            this.calculateToolStripMenuItem.Text = "Calculate";
+            this.calculateToolStripMenuItem.Click += new System.EventHandler(this.calculateToolStripMenuItem_Click);
+            // 
+            // clearToolStripMenuItem
+            // 
+            this.clearToolStripMenuItem.Name = "clearToolStripMenuItem";
+            this.clearToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.C)));
+            this.clearToolStripMenuItem.Size = new System.Drawing.Size(164, 22);
+            this.clearToolStripMenuItem.Text = "Clear";
+            this.clearToolStripMenuItem.Click += new System.EventHandler(this.clearToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.A)));
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // vehiclePriceCalc
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(331, 238);
+            this.Controls.Add(this.subtotalLabel);
+            this.Controls.Add(this.totalLabel);
+            this.Controls.Add(this.amountDueLabel);
+            this.Controls.Add(this.tradeInTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.customizedDetailingCheckBox);
+            this.Controls.Add(this.PearlizeCheckBox);
+            this.Controls.Add(this.standardCheckBox);
+            this.Controls.Add(this.computerNavigationCheckBox);
+            this.Controls.Add(this.basePriceTextBox);
+            this.Controls.Add(this.leatherInterionCheckBox);
+            this.Controls.Add(this.stereoSystemCheckBox);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "vehiclePriceCalc";
+            this.Text = "Vehicle Price Calculator";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox stereoSystemCheckBox;
+        private System.Windows.Forms.CheckBox leatherInterionCheckBox;
+        private System.Windows.Forms.TextBox basePriceTextBox;
+        private System.Windows.Forms.CheckBox computerNavigationCheckBox;
+        private System.Windows.Forms.CheckBox standardCheckBox;
+        private System.Windows.Forms.CheckBox PearlizeCheckBox;
+        private System.Windows.Forms.CheckBox customizedDetailingCheckBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tradeInTextBox;
+        private System.Windows.Forms.Label amountDueLabel;
+        private System.Windows.Forms.Label totalLabel;
+        private System.Windows.Forms.Label subtotalLabel;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem calculateToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clearToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
---
+++ ProcessingData/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProcessingData
+{
+    public partial class vehiclePriceCalc : Form
+    {
+        private double stereoSystem = 425.76;
+        private double leatherInterior = 987.41;
+        private double computerNavigation = 1741.23;
+        private double pearlize = 345.72;
+        private double customizedDetailing = 599.99;
+        private double taxRate = 0.08;
+        public vehiclePriceCalc()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            double subTotal = CalculateSubTotal();
+            double total = CalculateTotal(subTotal);
+            double totalDue = CalculateTotalDue(total);
+
+            subtotalLabel.Text += subTotal.ToString("C");
+            totalLabel.Text += total.ToString("C");
+            amountDueLabel.Text += totalDue.ToString("C");
+        }
+        private double CalculateSubTotal()
+        {
+            double subTotal = Convert.ToDouble(basePriceTextBox.Text);
+
+            if (stereoSystemCheckBox.Checked)
+            {
+                subTotal += stereoSystem;
+            }
+            if (leatherInterionCheckBox.Checked)
+            {
+                subTotal += leatherInterior;
+            }
+            if (computerNavigationCheckBox.Checked)
+            {
+                subTotal += computerNavigation;
+            }
+            if (PearlizeCheckBox.Checked)
+            {
+                subTotal += pearlize;
+            }
+            if (customizedDetailingCheckBox.Checked)
+            {
+                subTotal += customizedDetailing;
+            }
+
+            return subTotal;
+        }
+        private double CalculateTotal(double subTotal)
+        {
+            return subTotal + (subTotal * taxRate);
+        }
+        private double CalculateTotalDue(double total)
+        {
+            double tradeIn = Convert.ToDouble(tradeInTextBox.Text);
+            return total - tradeIn;
+        }
+
+        private void basePriceTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+
+            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void tradeInTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+
+            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void clearToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            basePriceTextBox.Text = "0";
+            tradeInTextBox.Text = "0";
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Calculates price due when purchasing a vehicle.");
+        }
+    }
+}
